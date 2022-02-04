00f299cf4c303e1b098aef04ba01c96c637bd05d
MIHIR-99251 <77870179+MIHIR-99251@users.noreply.github.com>
2022-02-04 08:51:34 +0530

    done

--- routes.js
+++ routes.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Movie = require('./Models/Movie')
 var User = require('./Models/User')
-var bcrypt = require('bcryptjs')
-var jwt = require('jsonwebtoken')
 
 //to fetch movies
 router.get('/movies',async(req,res)=>{
@@ -56,50 +54,10 @@ router.patch('/movies/:id',async (req,res)=>{
 })
 
 //delete api
-
-router.delete("/movies/:name",async(req,res)=>{
-    await Movie.deleteOne({name:req.params.name},(err,msg)=>{
-        if(err){
-            res.status(500).json({
-                error:err
-            })
-        }
-        else{
-            res.status(200).json({
-                msg:msg
-            })
-        }
-
-    })
-})
-
-
-router.post('/users',async(req,res)=>{
-    
-    //generate salt key
-    salt = await bcrypt.genSalt(10)
-    console.log(salt)
-
-    hashedpswd = await bcrypt.hash(req.body.password,salt)
-    console.log(hashedpswd)
-
-    const iuser = new User({
-        uname:req.body.uname,
-        password:hashedpswd
-    })  
-    await iuser.save((err,msg)=>{
-        if(err){
-            res.status(500).json({
-                "error":err
-            })
-        }
-        else{
-            res.status(200).json({
-                "My-message":msg
-            })
-        }
-    })
-
+router.delete('/movies/:id', async (req, res) => {   // delete by id
+    const _id = req.params.id;
+    const deleteItem = await Menu.findByIdAndDelete(_id);
+    res.send(deleteItem);
 })
 
 module.exports = router 
